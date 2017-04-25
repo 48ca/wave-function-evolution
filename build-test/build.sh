68adcedd0ca30612d853d53494bad9a6aa0ddcc5
James Houghton <jamesthoughton@gmail.com>
2017-04-25 18:19:26 -0400

    Update build script

--- build-test/build.sh
+++ build-test/build.sh
@@ -1,16 +1,40 @@
 #!/bin/bash
 
-set -e
-
 build() {
-	echo "Building with $2..."
+	echo -e "\e[96mBuilding with $2...\e[0m "
 	if [ -d $1 ]; then rm -r $1; fi
-	mkdir $1
-	pushd $1 > /dev/null
-	cmake ../.. $3 && make
-	popd > /dev/null
-	rm -rf $1
+	mkdir $1 && \
+	pushd $1 > /dev/null || return 1
+	cmake ../.. $3 > /dev/null || return 2
+	make > /dev/null || return 3
+	popd > /dev/null && \
+	rm -rf $1 || return 4
+	return 0
 }
 
-build default "default settings"
-build noquadmath "no quadmath" "-DNO_QUADMATH=True"
+check() {
+
+	ERRORS=("" # Indices corresponding to the return codes of build
+			"!!! Error in setup"
+			"!!! Error running cmake"
+			"!!! Error running make"
+			"!!! Error while cleaning up")
+
+	echo -e "\e[91m${ERRORS[$1]}\e[0m"
+
+	# To make this non-fatal, remove this line
+	exit $1
+}
+
+if [ -e ../CMakeCache.txt ]
+then
+	echo "!!! It looks like you have already built CMake in the directory above."
+	echo "    This build script will not work if CMake detects a previous build."
+	echo "    Run clean-cmake.sh in the directory above to fix this problem."
+	exit 1
+fi
+
+build default "default settings" || check $?
+build noquadmath "no quadmath" "-DNO_QUADMATH=True" || check $?
+
+echo -e "\e[92mAll builds completed\e[0m"
