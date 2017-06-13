cf7ef2d6489ec9849ee1ffc14897462fc835ccca
James Houghton <jamesthoughton@gmail.com>
2017-06-12 23:41:38 -0400

    Better profile script

--- profile.sh
+++ profile.sh
@@ -3,13 +3,13 @@
 if [ -z "$1" ]
 then
 	echo "Specify what you want to profile"
-	echo "ex: ./profile.sh ./evolvequ -n 100"
+	echo "ex: ./profile.sh evolvequ -n 100"
 	exit 1
 fi
 
 TMP=$(mktemp)
 EVOLVEOUT=$(mktemp)
-valgrind --tool=callgrind --callgrind-out-file=$TMP $@ -o $EVOLVEOUT
+valgrind --tool=callgrind --callgrind-out-file=$TMP ./$@ -o $EVOLVEOUT
 rm $EVOLVEOUT
 kcachegrind $TMP
 rm $TMP
